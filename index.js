be8c8ed21eccec5d123d035c75d0252661b99323
K <fieldprogrammable@gmail.com>
2018-07-19 00:12:35 +0900

    [未テスト] Slack Webhookを追加

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const rcloadenv = require('@google-cloud/rcloadenv')
+const rcloadenv = require('@google-cloud/rcloadenv');
+const { IncomingWebhook } = require('@slack/client');
+
 const config_names = ['webhook_url', 'new_message', 'empty_message'];
 exports.notify = (event, callback) => {
     const config = {};
     config_names.forEach(v => {
         config[v] = rcloadenv.getVariables(v, '');
     });
-
     const data = event.data;
-    // debug
-    console.log('event', event, 'config', config);
-    callback();
+    // notify
+    const body = `テストメッセージだよ！\r\n#event\r\n${event}\r\n#config\r\n${config}`;
+    const webhook = new IncomingWebhook(config.webhook_url);
+    webhook.send(body, (err, res) => {
+        if (err) { console.error(err); }
+        if (res) { console.error(res); }
+        callback();
+    });
 }; 
