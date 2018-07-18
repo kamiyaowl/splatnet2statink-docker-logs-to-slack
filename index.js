9cdbf888894f62e4d6987442dd13f49d77e8fa38
K <fieldprogrammable@gmail.com>
2018-07-19 00:08:36 +0900

    実行時変数読み込みを実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const rcloadenv = require('@google-cloud/rcloadenv')
+const config_names = ['webhook_url', 'new_message', 'empty_message'];
 exports.notify = (event, callback) => {
-    console.log(event);
+    const config = {};
+    config_names.forEach(v => {
+        config[v] = rcloadenv.getVariables(v, '');
+    });
+
+    const data = event.data;
+    // debug
+    console.log('event', event, 'config', config);
     callback();
 }; 
