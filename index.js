ce2dfe566bae5ee5272f4281bdfa771adcc866fc
fieldprogrammable <fieldprogrammable@gmail.com>
2018-07-20 03:42:13 -0400

    Fix env variable

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const rcloadenv = require('@google-cloud/rcloadenv');
 const { IncomingWebhook } = require('@slack/client');
 
 const config_names = ['webhook_url', 'new_message', 'empty_message', 'skip_empty'];
 exports.notify = (event, callback) => {
-    const config = {};
-    config_names.forEach(v => {
-        config[v] = rcloadenv.getVariables(v, '');
-    });
-    const data = event.data;
+    const config  = process.env;
     // notify
     const body = `テストメッセージだよ！\r\n#event\r\n${event}\r\n#config\r\n${config}`;
     const webhook = new IncomingWebhook(config.webhook_url);
