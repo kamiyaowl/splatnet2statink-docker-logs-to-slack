a3d421f2c093e386d8ab2a16608bcc142b2bf959
fieldprogrammable <fieldprogrammable@gmail.com>
2018-07-20 04:09:48 -0400

    Fix Test Message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config_names = ['webhook_url', 'new_message', 'empty_message', 'skip_empty
 exports.notify = (event, callback) => {
     const config  = process.env;
     // notify
-    const body = `テストメッセージだよ！\r\n#event\r\n${event}\r\n#config\r\n${config}`;
+    const body = `テストメッセージだよ！\r\n#event\r\n${JSON.stringify(event)}\r\n#config\r\n${JSON.stringify(config)}`;
     const webhook = new IncomingWebhook(config.webhook_url);
     webhook.send(body, (err, res) => {
         if (err) { console.error(err); }
