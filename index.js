42d4127c7f3d6135f299f957c5665cd9729edfe0
fieldprogrammable <fieldprogrammable@gmail.com>
2018-07-21 09:26:32 -0400

    Fix simple

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const { IncomingWebhook } = require('@slack/client');
 
-const config_names = ['webhook_url', 'new_message', 'empty_message', 'skip_empty'];
 exports.notify = (event, callback) => {
-    const config  = process.env;
-    // notify
-    const body = `テストメッセージだよ！\r\n#event\r\n${JSON.stringify(event)}\r\n#config\r\n${JSON.stringify(config)}`;
-    console.log(body);
+    const config = process.env;
+    const body = config.message;
     const webhook = new IncomingWebhook(config.webhook_url);
     webhook.send(body, (err, res) => {
         if (err) { console.error(err); }
-        if (res) { console.error(res); }
         callback();
     });
 }; 
