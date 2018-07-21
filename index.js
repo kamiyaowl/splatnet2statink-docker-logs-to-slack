b793a08672db8aabefee49d7f8ace02e1bc8bdf8
fieldprogrammable <fieldprogrammable@gmail.com>
2018-07-21 07:29:01 -0400

    Add Body log

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.notify = (event, callback) => {
     const config  = process.env;
     // notify
     const body = `テストメッセージだよ！\r\n#event\r\n${JSON.stringify(event)}\r\n#config\r\n${JSON.stringify(config)}`;
+    console.log(body);
     const webhook = new IncomingWebhook(config.webhook_url);
     webhook.send(body, (err, res) => {
         if (err) { console.error(err); }
