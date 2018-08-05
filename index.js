6f406adad8d5c8b22e188f5531d1ecbccc3a0fbe
K <kamiyaowl@gmail.com>
2018-08-05 17:02:51 +0900

    Implement #1 : textPayloadを出力するように変更

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { IncomingWebhook } = require('@slack/client');
 
 exports.notify = (event, callback) => {
     const config = process.env;
-    const body = config.message;
+    if (!event.data) { return; }
+    const text = event.data.textPayload;
+    if (!text) { return; }
+    const body = text;
     const webhook = new IncomingWebhook(config.webhook_url);
     webhook.send(body, (err, res) => {
         if (err) { console.error(err); }
