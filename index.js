bbb3535147e10a8aa52a4c3d52a42c7ff4ad0260
K <kamiyaowl@gmail.com>
2018-08-06 12:31:09 +0900

    Close #3 : event.data.dataをbase64デコードしてから使うように修正

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const { IncomingWebhook } = require('@slack/client');
 
+// KEY - dataから取り出すデータ名
+// WEBHOOL_URL - Webhook通知先
 exports.notify = (event, callback) => {
     const config = process.env;
+    const key = config.KEY || 'textPayload';
     // get textPayload
-    if (!event.data) { return; }
-    const text = event.data.textPayload;
-    if (!text) { return; }
-    const body = text;
+    if (!event.data) { 
+        console.error('data is null or empty');
+        callback();
+        return;
+     }
+    const dataStr = Buffer.from(event.data.data, 'base64').toString();
+    const data = JSON.parse(dataStr);
+    if (!data) {
+        console.error('data is null or empty');
+        callback();
+        return;
+    }
+    const text = data[key];
+    if (!data) {
+        console.error(`data[${key}] is null or empty`);
+        callback();
+        return;
+    }
     // notify
-    const webhook = new IncomingWebhook(config.webhook_url);
+    const body = text;
+    const webhook = new IncomingWebhook(config.WEBHOOL_URL);
     webhook.send(body, (err, res) => {
         if (err) { console.error(err); }
         callback();
